40318fc148a951c2c79f93a8fc9ae44a701fb53b
Pambali <91451814+Pambali@users.noreply.github.com>
2022-04-21 12:09:48 +0530

    Update pagination.js

--- pagination.js
+++ pagination.js
@@ -3,7 +3,7 @@ let totalnoPages = 100;
 let pages = 10;
 
 element.innerHTML = createPagination(totalnoPages, pages);
-export function createPagination(totalPages, page) {
+ function createPagination(totalPages, page) {
   let liTag = '';
   let active;
   let beforePage = page - 1;
